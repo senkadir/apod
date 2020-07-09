239e3821dc424e83941e0f19466f6e6c3fbaf6ae
Abdulkadir Şen <abdulkadir.sen@neyasis.com>
2020-07-09 13:01:29 +0300

    hotfix

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apod
 Astronomy picture of the day
+
+
+![image](https://user-images.githubusercontent.com/10263337/87026298-37bc1a00-c1e4-11ea-9007-55662be3b308.png)
--- src/Apod/App.xaml.cs
+++ src/Apod/App.xaml.cs
@@ -8,13 +8,14 @@ namespace Apod
         {
             InitializeComponent();
 
+            Current.Properties["api-key"] = "X9kVgfYjlDhPPfyJ6ZddbhNY4hOw052jIvoCNgU4";
+            Current.Properties["api-url"] = $"https://api.nasa.gov/planetary/apod";
+
             MainPage = new MainPage();
         }
 
         protected override void OnStart()
         {
-            Current.Properties["api-key"] = "X9kVgfYjlDhPPfyJ6ZddbhNY4hOw052jIvoCNgU4";
-            Current.Properties["api-url"] = $"https://api.nasa.gov/planetary/apod";
         }
 
         protected override void OnSleep()
